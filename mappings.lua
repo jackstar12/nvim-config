3462b732cb7df74e03fc20c92ea9082b4e57fa9c
jackstar12 <jkranawetter05@gmail.com>
2023-08-23 17:13:46 +0200

    harpoon

--- mappings.lua
+++ mappings.lua
@@ -1,3 +1,13 @@
+local mark = require("harpoon.mark");
+local ui = require("harpoon.ui");
+
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
 -- Mapping data with "desc" stored directly by vim.keymap.set().
 --
 -- Please use this mappings table to set keyboard mapping since this is the
@@ -18,11 +28,19 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<leader>a"] = {
+      mark.add_file, desc = "Add to harpoon"
+    },
+    ["<leader>1"] = { function() ui.nav_file(1) end },
+    ["<leader>2"] = { function() ui.nav_file(2) end },
+    ["<leader>3"] = { function() ui.nav_file(3) end },
+    ["<leader>4"] = { function() ui.nav_file(4) end },
+    ["<C-t>"] = { "<C-\\>" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
   t = {
     -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
+    ["<esc>"] = { "<C-\\><C-N>" },
   },
 }
